4d854eff5544d25a7919655c93eb3156022e5ecf
pitchayakit <pitchayakit.p@gmail.com>
2019-09-20 11:58:46 +0800

    Append csv write

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
 const fs = require('fs');
 const csv = require('csv-parser'); 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter; 
 
-const csvWriter = createCsvWriter({  
-  path: 'output.csv',
-  header: [
-  {id: 'word', title: 'Word'},
-  {id: 'count', title: 'Count'},
-  {id: 'file', title: 'File'},
-  ]
+let fields = ['word','count','file']
+let newLine= "\r\n";
+fs.writeFile('output.csv', fields+newLine, function (err) {
+  if (err) throw err;
+  console.log('file write');
 });
 
-let csv_data_object=[]
 fs.createReadStream('sample.csv')  
   .pipe(csv())
   .on('data', (row) => {
     let dir = 'txt'
-    
     let files = fs.readdirSync(dir);
     let word = row.word
     let regex = new RegExp("\\b("+ word + ")\\b","g");
     let count = 0;
+
     files.forEach(file => {
         data = fs.readFileSync(dir+'/'+file, 'utf8');
         count += (data.match(regex) || []).length;
-        let csv_data = {word: row.word,count: count,file: file}
-        csv_data_object.push(csv_data);
+        //let csv_data = {word: row.word,count: count,file: file}
+        let csv_data = row.word + ',' + count + ',' + file
+        fs.appendFile('output.csv', csv_data + newLine, function (err) {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file! : ' + csv_data);
+        });
     })
     console.log(count)
-  })
-  .on('end',()=> {
-    console.log(csv_data_object)
-    csvWriter.writeRecords(csv_data_object)
-      .then(()=> console.log('The CSV file was written successfully'));
   })
 
 
